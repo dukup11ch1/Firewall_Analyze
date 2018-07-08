2d984f7e837a97b16c0bbf3d98ea6e0df867106e
dukup11ch1 <dukup11ch1@gmail.com>
2018-07-08 16:13:19 +0900

    modified

--- log_bad_ip.py
+++ log_bad_ip.py
@@ -15,8 +15,7 @@ def read_space(): #스페이스 단위로 읽기위한 read 함수 새로만듦
 
     return a
 
-ip=[]#송신자 ip
-le=[]#ip별 전체 길이
+ip=['188.18.109.220', '187.205.239.58', '188.18.109.178', '190.153.50.5', '205.240.83.113', '200.96.202.149','190.12.110.198', '201.105.140.65', '189.101.254.77', '190.154.212.234', '188.18.252.214', '187.227.61.204', '189.103.81.30', '188.18.254.191', '188.18.110.213', '189.107.159.211', '190.16.153.49', '187.243.105.125', '187.250.101.20', '189.1.48.108', '190.113.88.71', '187.38.17.95']#송신자 ip
 inq=False#송신자 ip 중복검사
 inx=0#인덱스
 while True:
@@ -28,17 +27,17 @@ while True:
     for s in b:
         if "dst_ip" in s:
             temp=s.split('=')[1]
-            inq=True
+            '''inq=True
             if temp in ip:
                 inx=ip.index(temp)
                 continue
             ip.append(temp)#ip리스트에 없으면 추가
             le.append(0)
-            inx=ip.index(temp)
-        if "length" in s:
-            if inq:
-                inq=False
-                le[inx]+=int(s.split('=')[1])#length 계산
+            inx=ip.index(temp)'''
+            if temp in ip:
+                continue
+            print temp
+            
 
 print ip
 print le
